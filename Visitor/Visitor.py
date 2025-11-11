46ad2d83870cbaea82e59457d11b38d5031279bf
Samuel Leyton Muñoz <145390285+samuelleyton2006@users.noreply.github.com>
2025-11-10 22:59:40 -0500

    Implement arithmetic operations in DeepLearningVisitor
    Added imports and implemented arithmetic operations in DeepLearningVisitor.

--- Visitor/Visitor.py
+++ Visitor/Visitor.py
@@ -1,1 +1,14 @@
-./
+from libs.mylib_aritmetica import *
+from libs.mylib_matrices import *
+from libs.mylib_archivos import *
+from libs.mylib_funciones import *
+from libs.mylib_redes import *
+
+class DeepLearningVisitor(ParseTreeVisitor):
+    def visitOperacionAritmetica(self, ctx):
+        a = self.visit(ctx.expresion(0))
+        b = self.visit(ctx.expresion(1))
+        op = ctx.op.text
+        if op == '+': return suma(a, b)
+        elif op == '*': return multiplicacion(a, b)
+        elif op == '^': return potencia(a, b)
